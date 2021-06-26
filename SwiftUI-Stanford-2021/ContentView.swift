494087809aa6fa65ce0bbe569de9d5620756fca1
jubakong <jubakong@gmail.com>
2021-06-26 15:08:59 +0900

    first commit

---
+++ SwiftUI-Stanford-2021/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-Stanford-2021
+//
+//  Created by Jeongbae Kong on 2021/06/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
